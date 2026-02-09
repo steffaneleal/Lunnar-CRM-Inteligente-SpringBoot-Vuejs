02c233ef7478c47382a6fc7d1dd0155506160983
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2026-02-09 17:05:52 -0300

    :tada: Commit inicial Primeiro commit com a estrutura inicial do projeto (apenas o Spring Boot).

---
+++ src/test/java/com/steffaneleal/lunnar/LunnarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steffaneleal.lunnar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunnarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
